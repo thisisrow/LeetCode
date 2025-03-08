d9fa541335af14f18ae82634b2aeeb5f6e7be855
thisisrow <prathameshmishra2020@gmail.com>
2025-03-08 10:17:10 +0530

    Update searchInsert function to handle new test cases with modified input values

--- Arrary/Search Insert Position-35.js
+++ Arrary/Search Insert Position-35.js
@@ -1,12 +1,14 @@
-nums=[1,3,6];
-target=5;
+nums=[1,3,5,6];
+target=7;
 var searchInsert = function(nums, target) {
+
     for(i=0;i<nums.length;i++){
         if(nums[i]==target){
             return i;
         }else if(nums[i]>target){
             return i;
         }
+       return nums.length;
 }};
 
 console.log(searchInsert(nums, target)); 
