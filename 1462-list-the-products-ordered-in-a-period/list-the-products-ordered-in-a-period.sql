f772d6f6f3fa977140fa0e9d265782f612ea7cae
thisisrow <prathameshmishra2020@gmail.com>
2025-08-14 20:09:43 +0530

    Time: 698 ms (65.83%) Space: 0B (100.00%)

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,7 +1,6 @@
-SELECT p.product_name,SUM(o.unit) AS unit
+SELECT p.product_name AS product_name, SUM(o.unit) AS unit
 FROM Products p
-JOIN Orders o 
-USING (product_id)
+JOIN Orders o USING (product_id)
 WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02'
-GROUP BY  p.product_id
+GROUP BY p.product_id
 HAVING SUM(o.unit) >= 100;
