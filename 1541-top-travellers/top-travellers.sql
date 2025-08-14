35b134e8b33bad73f51795176dd39a642a7012dd
thisisrow <prathameshmishra2020@gmail.com>
2025-08-14 20:31:27 +0530

    Time: 886 ms (35.19%) Space: 0B (100.00%)

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select 
+u.name ,
+COALESCE(sum(r.distance),0) as travelled_distance
+from Users u
+left join Rides r
+on u.id = r.user_id
+GROUP BY u.id, u.name
+order by travelled_distance desc, u.name ;
